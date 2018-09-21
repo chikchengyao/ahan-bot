e9297df646ee2a27adb09e0301a40656a775f87b
chikchengyao <chikben@gmail.com>
2018-09-21 13:50:36 +0800

    Add second-order triggers

--- resources.py
+++ resources.py
@@ -1,17 +1,33 @@
 import re
 
+common_reply = ["Hoo my gosh this bot is too fucked",
+                "I swear i'm going to create an anti-spam-ahan bot that tells this bot to stfu",
+                "Hint: one of the above is an impersonator!!!",
+                "Im gnna sue for identity theft",
+                "OMG this bot needs to STFU"
+                "Im going thru an identity crises :(",
+                "Who the fuck is the real me???",
+                "I cant take this anymore guys",
+                "fcukign toxicc CS cullture",
+                "OMG fuck whoever mdde the bot",
+                "Its all a CS CONSPIRACY!!!",
+                "MMAAAAALLAAAAAAA",
+                "you noe wat time it is !!",
+                " fuk u nicSHAL :(((  ",
+                "somebody KICK THIS BOT"
+                ]
+
 # Hardcoding triggers and responses.
 # Tech related
 tech_buzzwords = re.compile(r"\bar\b|\bvr\b|\bblock[\s]*chain[sz]*\b|\bcrypto\b|\bmachine[\s]*learning\b|\bcloud\b|\bHD\b",
                             flags=re.IGNORECASE | re.MULTILINE)
-tech_buzzwords_reply = ["AR VR BLOCKCHAIN CRYTO CLOUD MACHINE LEARNING HHHHHHHHHD PORRRRNNNNNN!!!!!"]
+tech_buzzwords_reply = common_reply
 
 # Generic Ahan
 triggers = re.compile(r"\bstfu\b.*\bahan\b|\bfuck\b.*\bahan\b",
                       flags=re.IGNORECASE | re.MULTILINE)
-trigger_replies = ["dohohohoooont be a fucker lah OUI", "LULZ", "fk youuuu LULZ jk luv <3", "thx bb xoxo", "BODOH LA U", "YEEHEEEEEE"]
+trigger_replies = common_reply
 
 # Food / Ameens
 food_words= re.compile(r"\bsupper\b|\ba+m+e+n+[sz]*\b|\bmacs+\b|\bchee+se[\s]*fries+\b|\border(ing)?\b|\bma+la+\b", flags=re.IGNORECASE | re.MULTILINE)
-food_replies = ["so ameens???", "you noe what time it is right", "save some cheese fries for me plez <333",
-                "CHEEEESE FRIES", "now we HAVE to order", "fuck that, anyone wnna eat some MALA???"]
+food_replies = common_reply
